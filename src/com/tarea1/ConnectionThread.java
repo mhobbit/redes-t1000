5272f7a88b4975eda096ce1419c1e0009cd2d6b6
mhobbit <mhobbit@gmail.com>
2015-03-31 00:53:38 -0300

    comienzo cosas server
    faltan varias cosas, http://cs.au.dk/~amoeller/WWW/javaweb/server.html

---
+++ src/com/tarea1/ConnectionThread.java
@@ -0,0 +1,49 @@
+package com.tarea1;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ConnectionThread {
+    Socket connection;
+
+    public ConnectionThread(Socket connection) {
+        this.connection = connection;
+    }
+
+    public void runThread(){
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            OutputStream output = new BufferedOutputStream(connection.getOutputStream());
+            PrintStream printOutput = new PrintStream(output);
+
+            //leyendo el request
+            String[] request = input.readLine().split(" ");
+            input.close();
+
+            if(request.length != 3) {
+                errorReport(printOutput, connection, "400", "Bad Request", "Your browser sent a request that ");
+            }
+            //HTTP HANDLERS!
+            if(){
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void errorReport(PrintStream pout, Socket connection,
+                                    String code, String title, String msg) {
+        pout.print("HTTP/1.0 " + code + " " + title + "\r\n" +
+                "\r\n" +
+                "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\r\n" +
+                "<TITLE>" + code + " " + title + "</TITLE>\r\n" +
+                "</HEAD><BODY>\r\n" +
+                "<H1>" + title + "</H1>\r\n" + msg + "<P>\r\n" +
+                "<HR><ADDRESS>FileServer 1.0 at " +
+                connection.getLocalAddress().getHostName() +
+                " Port " + connection.getLocalPort() + "</ADDRESS>\r\n" +
+                "</BODY></HTML>\r\n");
+    }
+}
--- src/com/tarea1/Main.java
+++ src/com/tarea1/Main.java
@@ -1,8 +1,35 @@
 package com.tarea1;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Inicializando socket en puerto 8080
+        int port = 8080;
+        int route = "www"
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true){
+            Socket connection = null;
+            try {
+                connection = socket.accept();
+                if (connection != null) {
+                    //crear nuevo thread con la conexión
+
+                    connection = null;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
